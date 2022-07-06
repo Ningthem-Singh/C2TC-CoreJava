6b214a822a0868208e2817026b267afcfaafaee0
Ning_Kraken <ningthemsingh99@gmail.com>
2022-07-07 01:14:02 +0530

    string overall

--- TNS_Java_C2TC/src/com/tns/session/String.java
+++ TNS_Java_C2TC/src/com/tns/session/String.java
@@ -29,6 +29,22 @@ public class String {
 		System.out.println(s.substring(2));			//ignoring the first 2 values and printing out the rest
 		
 		System.out.println(s.substring(3,8));		//starting by ignoring the first 3 and printing only till the 8th position
+		
+		System.out.println("-----------------------------------------");
+		
+		java.lang.String a="Avantika ";
+
+		int b=10,c=20,d=30;
+	
+		
+		System.out.println(a+b+c+d);//Avantika102030 -------as it starts with a string and it concatenates b,c,d
+		
+		System.out.println(b+c+d+a);//60Avantika -----as it starts with an integer so 
+													//it adds all the integer and concatenates with the string
+		
+		System.out.println(b+c+a+d);//30Avantika30
+		
+		System.out.println(b+a+c+d);//10Avantika2030
 	}
 
 }
