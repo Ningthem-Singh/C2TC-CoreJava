0a9a269406660548475bee543acbe88118bf730f
Ning_Kraken <ningthemsingh99@gmail.com>
2022-07-06 22:42:09 +0530

    Signed-off-by: Ning_Kraken <ningthemsingh99@gmail.com>

---
+++ TNS_Java_C2TC/src/com/tns/session/AbstractDemo.java
@@ -0,0 +1,27 @@
+package com.tns.session;
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		Scooty Wego=new Scooty();	//we are using child class AKA scooty  
+									//and not parent class AKA Vehicle as parent class cannot be abstract  
+									//as abstract don't allow objects to be created. 
+		Wego.NoOfWheels();
+		
+	}
+}
+abstract class Vehicle   //partially implemented class as we are not specifying what vehicle it is
+{
+	abstract void NoOfWheels();
+	
+}
+
+ class Scooty extends Vehicle	//scooty is inheriting the parent class vehicle
+{
+
+	@Override
+	void NoOfWheels() {
+		System.out.println(2);
+	}
+	 
+}
