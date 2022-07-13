562a104007feaeb7fb33e0fd31515b655e172407
Ning_Kraken <ningthemsingh99@gmail.com>
2022-07-13 23:04:56 +0530

    Signed-off-by: Ning_Kraken <ningthemsingh99@gmail.com>

--- TNS_Java_C2TC/src/com/tns/session/Exp.java
+++ TNS_Java_C2TC/src/com/tns/session/Exp.java
@@ -14,6 +14,19 @@ public class Exp {
 			System.out.println(5);
 		}
 		System.out.println(6); // Outside Blocks
-	}
+		System.out.println("------------------");
 
+		try {
+			System.out.println(1);
+			System.out.println(2 / 0);
+			System.out.println(3);
+		}
+//		 catch (ArithmeticException e) {
+//			System.out.println("I am catch");
+//		}
+		finally {
+			System.out.println(5);
+		}
+		System.out.println(6); // Outside Blocks
+	}
 }
